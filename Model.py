a67a3091325818b635ad760613d7fd5560143fb4
Luke Shirley <Lukeshirley3@gmail.com>
2019-10-23 12:43:22 -0500

    WOP
    work in progress. succesful implementation of alphavantage api

--- Model.py
+++ Model.py
@@ -7,30 +7,13 @@ import matplotlib.pyplot as plt
 import seaborn
 
 #Data fetching
-import yfinance as yf
-#from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
 
-#Print tabular data
-from tabulate import tabulate
+#API key
+api_key = 'USYAK8KB5VER3M1P'
 
-#Calculate daily returns
-df = yf.download('GOOG')
-df = yf.download('MSFT')
-df = yf.download('AAPL')
-df['returns'] = df.Close.pct_change()
-df = df.dropna()
-plt.hist(df.returns, bins=40)
-plt.xlabel('Returns')
-plt.ylabel('Frequency')
-plt.grid(True)
-plt.show()
-
-#Sort the returns
-df.sort_values('returns', inplace = True, ascending = True)
-
-#Calculate Value at Risk
-VaR_90 = df['returns'].quantile(0.1)
-VaR_95 = df['returns'].quantile(0.05)
-VaR_99 = df['returns'].quantile(0.01)
-
-print (tabulate([['Confidence Level', 'Value at Risk'], ['90%', VaR_90], ['95%', VaR_95], ['99%', VaR_99]], headers ="firstrow"))
+#pull Data
+cc = CryptoCurrencies(key=api_key, output_format='pandas')
+data, meta_data = cc.get_digital_currency_daily(symbol='BTC', market='USD')
+data['4b. close (USD)']
+print(data)
